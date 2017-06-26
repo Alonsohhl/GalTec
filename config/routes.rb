84142b277a5f8de476fea8ceaec356528f06e4be
Alonso H. Huayta <alonso.hl25@gmail.com>
2017-06-25 21:39:54 -0500

    correcciones finales

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   #get 'welcome/index'
-  get '/about' ,  to:'static_pages#about'
+  get '/about' ,  to:'static_pages#about' 
   get '/contact' ,  to:'static_pages#contact'
   get '/news' ,  to:'static_pages#news'
   get '/services' ,  to:'static_pages#services'
