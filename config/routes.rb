d90d922c9c527778d6e86fa60ea46ec11c03cc8d
Alonso Huayta <alonso.hl25@gmail.com>
2017-07-19 15:05:23 -0500

    terminando blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
 
+  resources :posts
   #get 'static_pages/index'
 
   root 'static_pages#index'
 
   #get 'welcome/index'
-  get '/about' ,  to:'static_pages#about' 
-  get '/contact' ,  to:'static_pages#contact'
+  #get '/about' ,  to:'static_pages#about'
+  get '/nosotros' ,  to:'static_pages#about'
+
+  #get '/contact' ,  to:'static_pages#contact'
+  get '/contacto' ,  to:'static_pages#contact'
+
+  #get '/services' ,  to:'static_pages#services'
+  get '/servicios' ,  to:'static_pages#services'
+
+
+  #get '/index' ,  to:'static_pages#index'
   get '/news' ,  to:'static_pages#news'
-  get '/services' ,  to:'static_pages#services'
-  get '/index' ,  to:'static_pages#index'
 
-
+  get '/blog' ,  to:'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
