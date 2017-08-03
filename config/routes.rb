7bcef069a55b6b446b2111135100ccc0cc23d36b
Alonso Huayta <alonso.hl25@gmail.com>
2017-08-03 14:51:50 -0500

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  #mount Rich::Engine => '/rich', :as => 'rich'
+
   mount Ckeditor::Engine => '/ckeditor'
-  #mount Rich::Engine => '/rich', :as => 'rich'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :usuarios
   resources :posts
